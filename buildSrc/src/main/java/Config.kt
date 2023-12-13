296ee580b097bc159f5b3f6341b1b96e77c56b18
Marsel151205 <mambetalievmarsel2289@gmail.com>
2023-12-13 21:14:55 +0600

    Changed jvmTarget version

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -4,7 +4,7 @@ object Config {
     const val versionCode = 1
     const val versionName = "1.0"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    const val targetJvm = "1.8"
+    const val targetJvm = "17"
     const val consumerProguardFiles = "consumer-rules.pro"
 }
 
