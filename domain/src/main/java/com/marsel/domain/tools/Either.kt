d78a925e0f037ae1dd71c3a80aeeb745d823597b
Marsel151205 <mambetalievmarsel2289@gmail.com>
2023-12-14 23:20:03 +0600

    Created a sealed class for processing data coming from the api

---
+++ domain/src/main/java/com/marsel/domain/tools/Either.kt
@@ -0,0 +1,6 @@
+package com.marsel.domain.tools
+
+sealed class Either<out L, out R> {
+    data class Left<out L>(val message: L) : Either<L, Nothing>()
+    data class Right<out R>(val data: R) : Either<Nothing, R>()
+}
