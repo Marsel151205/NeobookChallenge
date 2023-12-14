bc95dc4cb03708b037b5bb5beaeaac39e3dfdf19
Marsel151205 <mambetalievmarsel2289@gmail.com>
2023-12-14 23:18:29 +0600

    Provides Retrofit Client and Product Category Api Service

--- app/src/main/java/com/marsel/neobookchallenge/di/NetworkModule.kt
+++ app/src/main/java/com/marsel/neobookchallenge/di/NetworkModule.kt
@@ -1,9 +1,22 @@
 package com.marsel.neobookchallenge.di
 
+import com.marsel.data.remote.RetrofitClient
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object NetworkModule {}
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofitClient() = RetrofitClient()
+
+    @Provides
+    @Singleton
+    fun provideProductCategoryApiService(retrofitClient: RetrofitClient) =
+        retrofitClient.provideProductCategoryApiService()
+}
