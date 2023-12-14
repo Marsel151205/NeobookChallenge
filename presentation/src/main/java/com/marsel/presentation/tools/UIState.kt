4c3a7885136e233b79f31415671b38c6407f411d
Marsel151205 <mambetalievmarsel2289@gmail.com>
2023-12-14 23:24:15 +0600

    Created a class in order to wrap the data coming from the backend

---
+++ presentation/src/main/java/com/marsel/presentation/tools/UIState.kt
@@ -0,0 +1,7 @@
+package com.marsel.presentation.tools
+
+sealed class UIState<T> {
+    class Loading<T> : UIState<T>()
+    class Error<T>(val error: String) : UIState<T>()
+    class Success<T>(val data: T) : UIState<T>()
+}
